01179da7262d8c8b81c4fb52cc62feabfb4d4c7c
Jesse Lind <jesseclind@gmail.com>
2025-09-03 22:05:15 -0400

    feat(submissions): add dragon name and description to submissions page

--- components/Submissions/Submissions.tsx
+++ components/Submissions/Submissions.tsx
@@ -211,6 +211,18 @@ const Submissions = () => {
                         <label htmlFor=''>Artist:</label>
                         <span>{dragon.creator}</span>
                       </div>
+                      {dragon.dragonName && (
+                        <div className={styles.row}>
+                          <label htmlFor=''>Dragon Name:</label>
+                          <span>{dragon.dragonName}</span>
+                        </div>
+                      )}
+                      {dragon.dragonDescription && (
+                        <div className={styles.row}>
+                          <label htmlFor=''>Dragon Description:</label>
+                          <span>{dragon.dragonDescription}</span>
+                        </div>
+                      )}
                     </div>
                     <button
                       onClick={() =>
